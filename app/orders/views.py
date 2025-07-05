3a6132bca51e8d4472f246cf5e218da01d7e7d2b
lilitstepanyan19 <lil-lid@mail.ru>
2025-07-05 20:29:30 +0400

    checbox  add

--- app/orders/views.py
+++ app/orders/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.db import transaction
@@ -28,8 +27,9 @@ class CreateOrderView(LoginRequiredMixin, FormView):
         try:
             with transaction.atomic():
                 user = self.request.user
-                cart_items = Cart.objects.filter(user=user)
-
+                selected_cart_ids = self.request.POST.getlist("selected_carts")
+                cart_items = Cart.objects.filter(user=user, id__in=selected_cart_ids)
+            
                 if cart_items.exists():
                     # Создать заказ
                     order = Order.objects.create(
@@ -45,7 +45,6 @@ class CreateOrderView(LoginRequiredMixin, FormView):
                         name=cart_item.product.name
                         price=cart_item.product.sell_price()
                         quantity=cart_item.quantity
-
 
                         if product.quantity < quantity:
                             raise ValidationError(f'Недостаточное количество товара {name} на складе\
@@ -63,90 +62,22 @@ class CreateOrderView(LoginRequiredMixin, FormView):
 
                     # Очистить корзину пользователя после создания заказа
                     cart_items.delete()
-                    
-
                     messages.success(self.request, 'Заказ оформлен!')
                     return redirect('payment:process', order_id=order.id)
                 else:
                     messages.warning(self.request, 'Корзина пуста. Невозможно оформить заказ.')
                 return redirect('orders:create_order')
+            
         except ValidationError as e:
             messages.success(self.request, str(e))
             return redirect('orders:create_order')
-        
-    
+
     def form_invalid(self, form):
         messages.error(self.request, 'Заполните все обязательные поля!')
         return redirect('orders:create_order')
-    
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Оформление заказа'
         context['order'] = True
         return context
-
-
-
-# @login_required
-# def create_order(request):
-#     if request.method == 'POST':
-#         form = CreateOrderForm(data=request.POST)
-#         if form.is_valid():
-#             try:
-#                 with transaction.atomic():
-#                     user = request.user
-#                     cart_items = Cart.objects.filter(user=user)
-
-#                     if cart_items.exists():
-#                         # Создать заказ
-#                         order = Order.objects.create(
-#                             user=user,
-#                             phone_number=form.cleaned_data['phone_number'],
-#                             requires_delivery=form.cleaned_data['requires_delivery'],
-#                             delivery_address=form.cleaned_data['delivery_address'],
-#                             payment_on_get=form.cleaned_data['payment_on_get'],
-#                         )
-#                         # Создать заказанные товары
-#                         for cart_item in cart_items:
-#                             product=cart_item.product
-#                             name=cart_item.product.name
-#                             price=cart_item.product.sell_price()
-#                             quantity=cart_item.quantity
-
-
-#                             if product.quantity < quantity:
-#                                 raise ValidationError(f'Недостаточное количество товара {name} на складе\
-#                                                        В наличии - {product.quantity}')
-
-#                             OrderItem.objects.create(
-#                                 order=order,
-#                                 product=product,
-#                                 name=name,
-#                                 price=price,
-#                                 quantity=quantity,
-#                             )
-#                             product.quantity -= quantity
-#                             product.save()
-
-#                         # Очистить корзину пользователя после создания заказа
-#                         cart_items.delete()
-
-#                         messages.success(request, 'Заказ оформлен!')
-#                         return redirect('user:profile')
-#             except ValidationError as e:
-#                 messages.success(request, str(e))
-#                 return redirect('orders:create_order')
-#     else:
-#         initial = {
-#             'first_name': request.user.first_name,
-#             'last_name': request.user.last_name,
-#             }
-
-#         form = CreateOrderForm(initial=initial)
-
-#     context = {
-#         'title': 'Home - Оформление заказа',
-#         'form': form,
-#         'order': True,
-#     }
-#     return render(request, 'orders/create_order.html', context=context)
