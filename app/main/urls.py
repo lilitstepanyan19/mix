384e3a08bba48be3e5b7e50fde4ba2b64c21f55a
lilitstepanyan19 <lil-lid@mail.ru>
2025-09-20 21:25:48 +0400

    add delivery

--- app/main/urls.py
+++ app/main/urls.py
@@ -8,6 +8,8 @@ app_name = 'main'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('about/', views.AboutView.as_view(), name='about'),
+    path('contact/', views.ContactView.as_view(), name='contact'),
+    path('delivery/', views.DeliveryView.as_view(), name='delivery'),
 ]
 
 # urlpatterns = [
--- app/main/views.py
+++ app/main/views.py
@@ -27,6 +27,28 @@ class AboutView(TemplateView):
         return context
     
 
+class ContactView(TemplateView):
+    template_name = 'main/contact.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Concect - Контакты'
+        context['content'] = "our contacts"
+        context['text_on_page'] = "email: , phone: , address: "
+        return context
+    
+
+class DeliveryView(TemplateView):
+    template_name = 'main/delivery.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Delivery - Доставка и оплата'
+        context['content'] = "Delivery and payment"
+        context['text_on_page'] = "For delivered call 0000..."
+        return context
+    
+
 # def index(request):
 
 #     context = {
