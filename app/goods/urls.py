19c347867af656264f401f4a1047a821b9eb1b39
lilitstepanyan19 <lil-lid@mail.ru>
2025-09-29 23:06:41 +0400

    index + catalog

--- app/goods/urls.py
+++ app/goods/urls.py
@@ -6,6 +6,7 @@ app_name = 'goods'
 
 urlpatterns = [
     path('search/', views.CatalogView.as_view(), name='search'),
+    path('', views.CatalogView.as_view(), name='category_all'),
     path('<slug:category_slug>/', views.CatalogView.as_view(), name='index'),
     path('product/<slug:product_slug>/', views.ProductView.as_view(), name='product'),
 ]
